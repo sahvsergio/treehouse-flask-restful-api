cd89041a6c18e6702ea2163c416ba51877ba2f45
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-09-04 19:36:16 -0500

    add reqparse

--- resources/courses.py
+++ resources/courses.py
@@ -1,12 +1,37 @@
 from flask import jsonify, Blueprint
-from flask_restful import  Resource, Api
+from flask_restful import  Resource, Api, reqparse, inputs
 #from flask.ext.restful import Resource
 
 import models
 
 class Courselist(Resource):
+
+    def __init__(self):
+        self.reqparse=reqparse.RequestParser()
+        self.reqparse.add_argument(
+            'title',
+            required=True,
+            help='No course title  provided,
+            location=['form', 'json'])
+
+        self.reqparse.add_argument(
+            'url',
+            required=True,
+            help='No course URL provided',
+            location=['form', 'json']
+            type=inputs.url
+            )
+            super().__init__()
+
+
     def get(self):
         return jsonify({'courses':[{'title':'Python basics'}]})
+
+    def post(self):
+        args= self.reqparse.parse_args()
+        models.Course.create(**args)
+        return jsonify({'courses':[{'title':'Python basics'}]})
+    
 
 class Course(Resource):
     def get(self, id):
